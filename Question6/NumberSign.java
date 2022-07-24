de01e125febcb4dd4e395b6496228bb1da8a29ff
Khalid7057 <108934841+Khalid7057@users.noreply.github.com>
2022-07-24 12:07:30 +0530

    Add files via upload

---
+++ Question6/NumberSign.java
@@ -0,0 +1,19 @@
+class NumberSign
+{
+	 public static void main(String[] args) 
+    	{
+        		int no=-59;
+        		if(no > 0)
+        		{
+            			System.out.println(no+" is a positive number");
+        		}
+        		else if(no < 0)
+        		{
+            			System.out.println(no+" is a negative number");
+        		}
+        		else
+        		{
+            			System.out.println(no+" is neither positive nor negative");
+        		}
+    	}
+}
