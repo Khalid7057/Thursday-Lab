1e9afeeb0bb89f3fc26e80f3f7b318ed2a4b74eb
Khalid7057 <108934841+Khalid7057@users.noreply.github.com>
2022-07-24 12:05:35 +0530

    Add files via upload

---
+++ Question5/Addition.java
@@ -0,0 +1,14 @@
+class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int y= 89 ;
+		int z =90 ;
+		int add = y+ z ;
+		System.out.println("The Addition Of y and z is "+add);
+
+	}
+
+}
