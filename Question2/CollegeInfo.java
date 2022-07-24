d6135ed273c07a0059b0dc049e1553a3d12ff1e5
Khalid7057 <108934841+Khalid7057@users.noreply.github.com>
2022-07-24 11:58:53 +0530

    Add files via upload

---
+++ Question2/CollegeInfo.java
@@ -0,0 +1,31 @@
+class CollegeInfo {
+
+	public static void main(String[] args) 
+	{
+		
+		String StudentName ="Khalid";
+		String Address ="Miraj ";
+		int Phonenumber =1145263789;
+		String Gender ="Male" ;
+		String DateOFBirth ="03/10/1999";
+		double height =5.6 ;
+		short Weight =53  ;
+		String Degree ="B.Tech";
+		int id =65412378 ;
+		
+		
+		System.out.println("The ID Of Student is "+id);
+		System.out.println("The Student Name is "+StudentName);
+		System.out.println("The Address Of Student "+Address);
+		System.out.println("The Phone Number Of Student Is "+Phonenumber);
+		System.out.println("Gender "+Gender);
+		System.out.println("The Date Of Birth Of Student Is "+DateOFBirth);
+		System.out.println("Height Of Student Is "+height);
+		System.out.println("The Weight Of Student Is "+Weight);
+		System.out.println("The Degree Of Student Is "+Degree);
+		
+		
+
+	}
+
+}
